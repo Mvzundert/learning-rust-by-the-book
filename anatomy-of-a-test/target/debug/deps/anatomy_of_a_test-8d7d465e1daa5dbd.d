b35d72f11d56757da646b51313e13b4c37aa9523
Marius van Zundert <marius.vanzundert@gmail.com>
2023-06-04 00:20:25 +0200

    Removed target dir from git

--- anatomy-of-a-test/target/debug/deps/anatomy_of_a_test-8d7d465e1daa5dbd.d
+++
@@ -1,7 +0,0 @@
-/home/marske/projects/learning-rust-by-the-book/anatomy-of-a-test/target/debug/deps/anatomy_of_a_test-8d7d465e1daa5dbd.rmeta: src/lib.rs
-
-/home/marske/projects/learning-rust-by-the-book/anatomy-of-a-test/target/debug/deps/libanatomy_of_a_test-8d7d465e1daa5dbd.rlib: src/lib.rs
-
-/home/marske/projects/learning-rust-by-the-book/anatomy-of-a-test/target/debug/deps/anatomy_of_a_test-8d7d465e1daa5dbd.d: src/lib.rs
-
-src/lib.rs:
--- anatomy-of-a-test/target/debug/deps/anatomy_of_a_test-dd029937f2623850.d
+++
@@ -1,5 +0,0 @@
-/home/marske/projects/learning-rust-by-the-book/anatomy-of-a-test/target/debug/deps/anatomy_of_a_test-dd029937f2623850.rmeta: src/lib.rs
-
-/home/marske/projects/learning-rust-by-the-book/anatomy-of-a-test/target/debug/deps/anatomy_of_a_test-dd029937f2623850.d: src/lib.rs
-
-src/lib.rs:
